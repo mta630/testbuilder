0c2a9f8b2f63b2ac569ff9deaaf2b9dd6910aeed
Michael Albert <michael.t.albert89@gmail.com>
2020-02-18 17:29:07 -0600

    Final version with China UnionPay and Switch added

--- detectNetwork.js
+++ detectNetwork.js
@@ -24,6 +24,7 @@ var detectNetwork = function(cardNumber) {
   var length = cardNumber.length;
   var firstFourDigits = Number(cardNumber.substr(0,4));
   var firstTwoDigits = Number(cardNumber.substr(0,2));
+  var firstSixDigits = Number(cardNumber.substr(0,6));
 
 
   // Conditional statements for card numbers that dont start with a specific digit
@@ -39,6 +40,19 @@ var detectNetwork = function(cardNumber) {
   	return "Diner's Club";
   	} else if ((secondDigit === 4 || secondDigit === 7) && length === 15){
   	return "American Express";
+  	}
+  }
+
+  // Conditional statements for Switch
+  // Switch always has a prefix of 4903, 4905, 4911, 4936, 564182, 633110, 6333, or 6759 and a length of 16, 18, or 19."
+  var switchPrefixArray = ['4903', '4905', '4911', '4936', '564182', '633110', '6333', '6759'];
+
+  for (let i = 0; i < switchPrefixArray.length; i ++){
+  	let actualPrefix = cardNumber.substr(0,switchPrefixArray[i].length);
+  	if (length === 16 || length === 18 || length === 19){
+  		if (actualPrefix === switchPrefixArray[i]){
+  		return "Switch";
+  		}
   	}
   }
 
@@ -65,6 +79,30 @@ var detectNetwork = function(cardNumber) {
 
   if ((length > 11 && length < 20) && (firstFourDigits === 5018 || firstFourDigits === 5020 || firstFourDigits === 5038 || firstFourDigits === 6304)){
   	return "Maestro";
+  }
+
+  // China UnionPay always has a prefix of 622126-622925, 624-626, or 6282-6288 and a length of 16-19
+  var chinaPrefixArray = [];
+
+  for (let i = 622126; i <= 622925; i ++){
+  	chinaPrefixArray.push(i.toString());
+  }
+
+  for (let i = 624; i <= 626; i ++){
+  	chinaPrefixArray.push(i.toString());
+  }
+
+  for (let i = 6282; i <= 6288; i ++){
+  	chinaPrefixArray.push(i.toString());
+  }
+
+  for (let i = 0; i < chinaPrefixArray.length; i ++){
+  	let actualPrefix = cardNumber.substr(0,chinaPrefixArray[i].length);
+  	if (length <= 19 && length >= 16){
+  		if (actualPrefix === chinaPrefixArray[i]){
+  		return "China UnionPay";
+  		}
+  	}
   }
 
   // in any other case we will need to return invalid card number
